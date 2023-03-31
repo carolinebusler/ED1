932163d9584ef2c23b8e72a2fc93d037ecd9532e
carolinebusler <113452668+carolinebusler@users.noreply.github.com>
2023-03-31 08:28:08 -0400

    reflection fixes

---
+++ Synth/sketch.js
@@ -0,0 +1,33 @@
+//customize # of squares in grid
+
+let gif;
+function preload(){
+  gif = loadImage('assets/blueTriangles.gif');
+}
+
+
+function setup() { //runs one time
+createCanvas(windowWidth, windowWidth);
+background(0,0, 200); //navy blue
+}
+
+function draw() {//loop
+fill(0,200,255);//light blue
+strokeWeight(1);//stroke 20 pixels
+
+var num = 2; //# squares in grid
+var sideLen = windowWidth/num; //side length
+
+translate (-150, -150);//gives bleed effect
+
+for (var y=0; y<2 * windowWidth; y=y+sideLen){//builds columns
+for (var x=0; x< 2 *windowWidth; x=x+sideLen){//builds row
+image (gif, x,y, windowWidth/num, windowWidth/num);
+}
+}
+
+}
+
+function windowResized(){ //resizes and refreshes
+  resizeCanvas (windowWidth, windowHeight);
+}
